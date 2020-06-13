dd5d20bcb5fa3e5a260713272dce23d0d071fe89
mark palie <markpalie@gmail.com>
2020-06-12 19:36:45 -0500

    Modified test_calculator.py with 2 failures

--- test_calculator.py
+++ test_calculator.py
@@ -7,7 +7,7 @@ import calculator
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == calculator.add(2, 2)
+        assert 5 == calculator.add(2, 2)
 
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4, 2)
+        assert 3 == calculator.subtract(4, 2)
